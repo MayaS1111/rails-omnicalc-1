12b9755a13b79aa56fc79d1d95b29bc01b13eff6
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-05 16:15:55 +0000

    added routes, and started controller.

---
+++ app/controllers/calculate_controller.rb
@@ -0,0 +1,13 @@
+class CalculateController < ApplicationController
+  def home
+    render(template: "display/home")
+  end
+  def square 
+  end
+  def square_root
+  end
+  def payment
+  end
+  def random
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get("/", {controller: "calculate", action: "home"})
+  get("/square/new", {controller: "calculate", action: "square"})
+  get("/square_root/new", {controller: "calculate", action: "square_root"})
+  get("/payment", {controller: "calculate", action: "payment"})
+  get("/random", {controller: "calculate", action: "random"})
 end
