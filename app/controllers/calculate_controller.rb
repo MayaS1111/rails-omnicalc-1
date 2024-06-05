d812290de8c426bbc66c37b1f5f209e005ae1fbc
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-05 17:07:27 +0000

    Done!

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -9,6 +9,31 @@ class CalculateController < ApplicationController
     render({template: "displays/payment_new"})
   end
   def random
-    render({template: "displays/rand_new"})
+    render({template: "displays/random_new"})
+  end
+
+  def square_results
+    render({template: "displays/square_results"})
+  end
+  def square_root_results
+    render({template: "displays/square_root_results"})
+  end
+  def payment_results
+    @apr = params.fetch("users_number_apr").to_i.to_fs(:percentage, { :precision => 4 })
+    @years = params.fetch("users_number_years").to_i
+    @principal = params.fetch("users_number_principal").to_i.to_fs(:currency)
+
+    r = params.fetch("users_number_apr").to_f / 1200
+    n = params.fetch("users_number_years").to_i * 12
+    pv = params.fetch("users_number_principal").to_i
+
+    numerator = r*(pv)
+    denominator = 1 - (1 + r)**-n
+
+    @payment = (numerator / denominator).to_fs(:currency)
+    render({template: "displays/payment_results"})
+  end
+  def random_results
+    render({template: "displays/random_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get("/", {controller: "calculate", action: "square"})
   get("/square/new", {controller: "calculate", action: "square"})
   get("/square_root/new", {controller: "calculate", action: "square_root"})
-  get("/payment", {controller: "calculate", action: "payment"})
-  get("/random", {controller: "calculate", action: "random"})
+  get("/payment/new", {controller: "calculate", action: "payment"})
+  get("/random/new", {controller: "calculate", action: "random"})
+
+  get("/square/results", {controller: "calculate", action: "square_results"})
+  get("/square_root/results", {controller: "calculate", action: "square_root_results"})
+  get("/payment/results", {controller: "calculate", action: "payment_results"})
+  get("/random/results", {controller: "calculate", action: "random_results"})
 end
